d2f0fea5f7d41759e00a655e21305b8b4224bc2a
ArtemKamenskiy <artemkamenskiy98@gmail.com>
2020-05-25 04:27:33 +0300

    Routing

--- react-social/src/App.js
+++ react-social/src/App.js
@@ -3,17 +3,30 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <div className="app-container">
-          <Navbar/>
-          <Profile name="UserName" age="18" city="Some City"/>
-      </div>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <div className="app-container">
+                    <Navbar/>
+                    <div className='page-container'>
+                        <Route path='/profile' component={Profile} />
+                        <Route path='/dialogs' component={Dialogs} />
+                        <Route path='/news' component={News} />
+                        <Route path='/music' component={Music} />
+                        <Route path='/settings' component={Settings} />
+                    </div>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- react-social/src/components/Navbar/Navbar.jsx
+++ react-social/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import navClasses from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
+
 const Navbar = () => {
     return (
         <div className={navClasses.nav}>
             <ul>
-                <li className={navClasses.userProfile}><a>Profile</a></li>
-                <li className={navClasses.messages}><a>Messages</a></li>
-                <li className={navClasses.news}><a>News</a></li>
-                <li className={navClasses.music}><a>Music</a></li>
-                <li className={navClasses.settings}><a>Settings</a></li>
+                <li>
+                    <NavLink to='/profile' className={navClasses.userProfile}
+                             activeClassName={navClasses.active}>Profile</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/dialogs' className={navClasses.messages}
+                             activeClassName={navClasses.active}>Messages</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/news' className={navClasses.news} activeClassName={navClasses.active}>News</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/music' className={navClasses.music}
+                             activeClassName={navClasses.active}>Music</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/settings' className={navClasses.settings}
+                             activeClassName={navClasses.active}>Settings</NavLink>
+                </li>
             </ul>
         </div>
     )
